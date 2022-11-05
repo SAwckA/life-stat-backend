f28381e411b86bb4b1deabc674d2166fd75d3b28
alexander <saw.cka@mail.ru>
2022-11-06 06:01:49 +1000

    dev cors

--- lifestat/app.py
+++ lifestat/app.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI
 from fastapi.responses import Response
 from fastapi.requests import Request
 
+import settings.config as cfg
+
 from lifestat.database import database_class
 
 
@@ -12,25 +14,26 @@ app = FastAPI()
 db = database_class()
 
 
-@app.middleware('http')
-async def options_plug(request: Request, call_next):
-    if request.method == "OPTIONS":
-        response = Response()
+if cfg.DB_ENV == "DEV":
+    @app.middleware('http')
+    async def options_plug(request: Request, call_next):
+        if request.method == "OPTIONS":
+            response = Response()
+            response.headers['Access-Control-Allow-Origin'] = 'http://0.0.0.0:5173'
+            response.headers['Access-Control-Allow-Credentials'] = 'true'
+            response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
+            response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
+            response.status_code = 204
+            return response
+        
+        response = await call_next(request)
+        
         response.headers['Access-Control-Allow-Origin'] = 'http://0.0.0.0:5173'
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
         response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
-        response.status_code = 204
+        
         return response
-    
-    response = await call_next(request)
-    
-    response.headers['Access-Control-Allow-Origin'] = 'http://0.0.0.0:5173'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Authorization, Content-Type'
-    
-    return response
 
 
 import lifestat.routes  # noqa # for
